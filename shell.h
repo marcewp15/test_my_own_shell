40caf30d551a988b4499bd99f4d3aae79aec8449
PilarPinto <piapintoch@unal.edu.co>
2019-11-27 13:01:10 +0000

    [Add]

--- shell.h
+++ shell.h
@@ -1,9 +1,17 @@
 #ifndef SHELL_H
 #define SHELL_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
 char *_error(char *namefile, char **buf);
 int _strlen(char *s);
 void _prompt(void);
+char **splitInput(char *array);
+void duplicateProcess(char *array, char **space);
+void enviromentShell(char **space);
+void exitof(char **space, char *array);
+
 #endif

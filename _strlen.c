f6060182d45191a5a9c96088cfffcce917a0c71f
arcewp15 <970@holbertonschool.com>
2019-11-26 19:13:59 -0500

    [Add] the function that finds the lenght

---
+++ _strlen.c
@@ -0,0 +1,17 @@
+#include "shell.c"
+/**
+ * _strlen - length of string
+ *@s: input pointer
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != 00)
+	{
+		i++;
+	}
+	return (i);
+}
